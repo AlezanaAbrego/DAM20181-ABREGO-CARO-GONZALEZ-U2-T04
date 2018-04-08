81ccf046bdaa9c5030ac93f3d1fb98e492879de4
Ricardo <luricarolo@ittepic.edu.mx>
2018-04-08 10:15:10 -0600

    creating the method add in typescrytp

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -11,11 +11,55 @@ export class HomePage {
   public todos: any = [];
 
   constructor(
-    public navCtrl: NavController) {
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    public storage: Storage) {
 
   }
 
+  ionViewDidEnter() {
+    this.storage.get('todos').then((todos) => {
+      if (!todos) return;
+      this.todos = todos;
+    });
+  }
 
+
+  add() {
+    let alert = this.alertCtrl.create({
+      title: 'Nueva Tarea',
+      message: '¿Qué deseas hacer?',
+      inputs: [
+        {
+          name: 'nombre',
+          placeholder: 'Nombre'
+        },
+        {
+          name: 'fecha',
+          type: 'date'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Aceptar',
+          handler: (todo: any) => {
+            this.todos.push({ nombre: todo.nombre, fecha: todo.fecha, hecho: false });
+            this.storage.set('todos', this.todos);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+
+  save(todo) {
+    this.storage.set('todos', this.todos);
+  }
 
 
 }
