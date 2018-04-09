81d9b9fd1714b85d2960fedbe9886a9f20c107e3
EFabrizio <fabriziocastellanos@hotmail.com>
2018-04-08 22:23:11 -0600

    Add method home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 
@@ -22,6 +22,60 @@ export class HomePage {
       if (!todos) return;
       this.todos = todos;
     });
+  }
+
+update(todo, i) {
+    let alert = this.alertCtrl.create({
+      title: 'Modificar Tarea',
+      message: '¿Qué deseas hacer?',
+      inputs: [
+        {
+          name: 'nombre',
+          placeholder: 'Nombre',
+          value: todo.nombre
+        },
+        {
+          name: 'fecha',
+          type: 'date',
+          value: todo.fecha
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Aceptar',
+          handler: (data: any) => {
+            this.todos[i] = { nombre: data.nombre, fecha: data.fecha, hecho: todo.hecho };
+            this.save(todo);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  delete(todo, i) {
+    let alert = this.alertCtrl.create({
+      title: 'Eliminar Tarea',
+      message: ¿Seguro que deseas eliminar ${todo.nombre}?,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.todos.splice(i, 1);
+            this.save(todo);
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 
